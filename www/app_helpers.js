2ce4ed1da37a4e45c345673a8625fd2dca4f5298
Jason Chase <jasonpchase@gmail.com>
2015-04-28 12:55:58 -0400

    Update JS dependency requires to use NPM plugin ids

--- www/app_helpers.js
+++ www/app_helpers.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var helpers = require('org.chromium.common.helpers');
+var helpers = require('cordova-plugin-chrome-apps-common.helpers');
 
 var uiWindowFirstCallError = null;
 
